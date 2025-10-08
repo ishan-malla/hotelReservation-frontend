e869a74849527e851400bf201148239992699e97
ishan <saberktm90@gmail.com>
2025-10-08 21:02:41 +0545

    added login form and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
 import Landing from "./pages/Landing/Landing";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />}></Route>
       <Route path="/rooms" element={<HomePage />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   );
 };
---
+++ src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/components/LoginForm";
+
+const Login = () => {
+  return (
+    <div className="w-screen h-screen">
+      <LoginForm></LoginForm>
+    </div>
+  );
+};
+export default Login;
